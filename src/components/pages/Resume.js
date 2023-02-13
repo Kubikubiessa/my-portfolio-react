89ba14e8f88d1f7825c4a13e4ce757d558757519
Heidrun Kubiessa <heidrunkubiessa@Heidruns-MacBook-Air.local>
2023-02-13 14:31:34 -0700

    changed file path

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,7 +7,7 @@ function Resume() {
 				<h1 className="page-header">My Resume</h1>
 			</div>
 			<div className="bottom-spacing">
-				<a href={require("/Users/heidrunkubiessa/Bootcamp/repos/weekly-challenges/my-portfolio-react/src/assets/Heidrun Kubiessa Resume EMBA .pdf")} download>
+				<a href={require("../../assets/Heidrun Kubiessa Resume EMBA .pdf")} download>
 					<h4>Download my Resume</h4>
 				</a>
 			</div>
