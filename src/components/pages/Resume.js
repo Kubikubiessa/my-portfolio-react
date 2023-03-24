059848cc2bd569d0caa28a0a839da785508c42ea
Heidrun Kubiessa <heidrun.kubiessa@utah.edu>
2023-03-24 12:26:50 -0600

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,7 +7,7 @@ function Resume() {
 				<h1 className="page-header">My Resume</h1>
 			</div>
 			<div className="bottom-spacing">
-				<a href={require("../../assets/Heidrun Kubiessa Resume EMBA .pdf")} download>
+				<a href={require("../../assets/Heidrun Kubiessa Resume.pdf")} download>
 					<h4>Download my Resume</h4>
 				</a>
 			</div>
